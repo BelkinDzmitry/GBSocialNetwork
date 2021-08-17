12e173a114a97e30fc83433d5722192e31a222e5
Belkin Dzmitry <belkind@tut.by>
2021-08-17 09:02:48 +0300

    lesson2

---
+++ GBSocialNetwork/GBSocialNetwork/Presentation/MyGroups/Controller/MyGroupsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MyGroupsViewController.swift
+//  GBSocialNetwork
+//
+//  Created by Belkin on 15.08.2021.
+//
+
+import UIKit
+
+class MyGroupsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
